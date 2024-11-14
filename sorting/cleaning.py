e51361c717ae5136ffab6aee80ebfc347c1d703c
Alexander Zhao <zhaotianxiao722@icloud.com>
2024-11-13 23:07:26 -0500

    new datasets

--- sorting/cleaning.py
+++ sorting/cleaning.py
@@ -14,5 +14,18 @@ def cut_csv_rows_by_half(input_csv_path, output_csv_path):
     df_half.to_csv(output_csv_path, index=False)
 
 
-cut_csv_rows_by_half('../all-data.csv', '../all-data.csv')
-cut_csv_rows_by_half('../dataset.csv', '../dataset.csv')
+# cut_csv_rows_by_half('../all-data.csv', '../all-data.csv')
+# cut_csv_rows_by_half('../dataset.csv', '../dataset.csv')
+
+def remove_unwanted_columns(input_csv_path, output_csv_path):
+    # Read the CSV file
+    df = pd.read_csv(input_csv_path, encoding='ISO-8859-1')
+    
+    # Keep only the specified columns
+    df_filtered = df[['Date', 'Impact', 'Subject']]
+    
+    # Save the new dataframe to a new CSV file
+    df_filtered.to_csv(output_csv_path, index=False)
+
+
+remove_unwanted_columns('../dataset.csv', '../dataset.csv')
